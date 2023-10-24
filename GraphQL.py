76e6f17dd72ba1736b6a649505d9ab6119b7d5dc
ChowdhurySalehaFerdowsy <shipu9300@gmail.com>
2023-10-23 20:17:05 -0400

    changed

--- GraphQL.py
+++ GraphQL.py
@@ -7,10 +7,10 @@ class Stock(graphene.ObjectType):
     name = graphene.String()
     ticker = graphene.String()
     price = graphene.Float()
-    historical_prices = graphene.List(graphene.Float)
-    highest_price = graphene.Float()
-    lowest_price = graphene.Float()
-    trading_volume = graphene.Float()
+    historicalPrices = graphene.List(graphene.Float)
+    highestPrice = graphene.Float()
+    lowestPrice = graphene.Float()
+    tradingVolume = graphene.Float()
 
 
 # Define a list of stocks
@@ -19,28 +19,28 @@ stocks = [
         "name": "Company A",
         "ticker": "A",
         "price": 100.0,
-        "historical_prices": [95.0, 105.0, 110.0, 90.0, 100.0],
-        "highest_price": 110.0,
-        "lowest_price": 90.0,
-        "trading_volume": 1000000.0
+        "historicalPrices": [95.0, 105.0, 110.0, 90.0, 100.0],
+        "highestPrice": 110.0,
+        "lowestPrice": 90.0,
+        "tradingVolume": 1000000.0
     },
     {
         "name": "Company B",
         "ticker": "B",
         "price": 50.0,
-        "historical_prices": [45.0, 55.0, 60.0, 40.0, 50.0],
-        "highest_price": 60.0,
-        "lowest_price": 40.0,
-        "trading_volume": 500000.0
+        "historicalPrices": [45.0, 55.0, 60.0, 40.0, 50.0],
+        "highestPrice": 60.0,
+        "lowestPrice": 40.0,
+        "tradingVolume": 500000.0
     },
     {
         "name": "Company C",
         "ticker": "C",
         "price": 75.0,
-        "historical_prices": [70.0, 80.0, 85.0, 65.0, 75.0],
-        "highest_price": 85.0,
-        "lowest_price": 65.0,
-        "trading_volume": 750000.0
+        "historicalPrices": [70.0, 80.0, 85.0, 65.0, 75.0],
+        "highestPrice": 85.0,
+        "lowestPrice": 65.0,
+        "tradingVolume": 750000.0
     }
 ]
 
@@ -55,10 +55,10 @@ class Query(graphene.ObjectType):
                     name=stock['name'],
                     ticker=stock['ticker'],
                     price=stock['price'],
-                    historical_prices=stock['historical_prices'],
-                    highest_price=stock['highest_price'],
-                    lowest_price=stock['lowest_price'],
-                    trading_volume=stock['trading_volume']
+                    historicalPrices=stock['historicalPrices'],
+                    highestPrice=stock['highestPrice'],
+                    lowestPrice=stock['lowestPrice'],
+                    tradingVolume=stock['tradingVolume']
                 )
         return None
 
